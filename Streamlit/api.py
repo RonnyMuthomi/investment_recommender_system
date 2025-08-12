cc8f2531b257427a9266573f28f56b570c63d441
Faith Karimi <muthonikarimif@gmail.com>
2025-08-12 11:36:39 +0300

    Update api.py

--- Streamlit/api.py
+++ Streamlit/api.py
@@ -207,4 +207,4 @@ async def global_exception_handler(request, exc):
     }
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run(app, host="localhost", port=8000, reload=False)
